5b6b7250b234a01f0d769fcfb97c77d00796cc02
Ben Cornwell-Mott <bcornwellmott@velometro.com>
2016-07-31 06:06:58 -0700

    Fixed subcontracting selection

--- erpnext/manufacturing/doctype/production_planning_tool/production_planning_tool.py
+++ erpnext/manufacturing/doctype/production_planning_tool/production_planning_tool.py
@@ -313,10 +313,10 @@ class ProductionPlanningTool(Document):
 			}
 		"""
 		item_list = []
-
+		
 		for bom, so_wise_qty in bom_dict.items():
 			bom_wise_item_details = {}
-			if self.use_multi_level_bom:
+			if self.use_multi_level_bom and self.only_raw_materials:
 				# get all raw materials with sub assembly childs
 				# Did not use qty_consumed_per_unit in the query, as it leads to rounding loss
 				for d in frappe.db.sql("""select fb.item_code,
@@ -324,29 +324,43 @@ class ProductionPlanningTool(Document):
 					fb.description, fb.stock_uom, item.min_order_qty
 					from `tabBOM Explosion Item` fb, `tabBOM` bom, `tabItem` item
 					where bom.name = fb.parent and item.name = fb.item_code
-					and (item.is_sub_contracted_item = 0 or ifnull(item.default_bom, "")="")
+					and (item.is_sub_contracted_item = %(subcontracted)s or ifnull(item.default_bom, "")="")
 					and item.is_stock_item = 1
 					and fb.docstatus<2 and bom.name=%s
-					group by fb.item_code, fb.stock_uom""", bom, as_dict=1):
+					group by fb.item_code, fb.stock_uom""", {"bom":bom, "subcontracted":self.include_subcontracted}, as_dict=1):
 						bom_wise_item_details.setdefault(d.item_code, d)
 			else:
 				# Get all raw materials considering SA items as raw materials,
 				# so no childs of SA items
-				for d in frappe.db.sql("""select bom_item.item_code,
-					ifnull(sum(bom_item.qty/ifnull(bom.quantity, 1)), 0) as qty,
-					bom_item.description, bom_item.stock_uom, item.min_order_qty
-					from `tabBOM Item` bom_item, `tabBOM` bom, tabItem item
-					where bom.name = bom_item.parent and bom.name = %s and bom_item.docstatus < 2
-					and bom_item.item_code = item.name
-					and item.is_stock_item = 1
-					group by bom_item.item_code""", bom, as_dict=1):
-						bom_wise_item_details.setdefault(d.item_code, d)
+				bom_wise_item_details = self.get_subitems(bom_wise_item_details, bom,1, self.use_multi_level_bom, self.include_subcontracted)
+				
 			for item, item_details in bom_wise_item_details.items():
 				for so_qty in so_wise_qty:
 					item_list.append([item, flt(item_details.qty) * so_qty[1], item_details.description,
 						item_details.stock_uom, item_details.min_order_qty, so_qty[0]])
-
+						
 		self.make_items_dict(item_list)
+
+	def get_subitems(self,bom_wise_item_details, bom, parent_qty, include_sublevel, supply_raw):
+		for d in frappe.db.sql("""select bom_item.item_code,
+						ifnull(%(parent_qty)s * sum(bom_item.qty/ifnull(bom.quantity, 1)), 0) as qty,
+						item.description, item.stock_uom, item.min_order_qty,  item.is_sub_contracted_item, item.default_bom
+						from `tabBOM Item` bom_item, `tabBOM` bom, tabItem item
+						where bom.name = bom_item.parent and bom.name = %(bom)s and bom_item.docstatus < 2
+						and bom_item.item_code = item.name
+						and item.is_stock_item = 1
+						group by bom_item.item_code""", {"bom": bom, "parent_qty": parent_qty}, as_dict=1):
+							if d.item_code in bom_wise_item_details:
+								bom_wise_item_details[d.item_code] += d.qty
+							else:
+								bom_wise_item_details.setdefault(d.item_code, d)
+								
+							if include_sublevel:
+								if (d.default_material_request_type == "Purchase" and d.is_sub_contracted_item and supply_raw) or (d.default_material_request_type == "Manufacture"):
+									child_details = self.get_subitems(bom_wise_item_details,d.default_bom,d.qty, include_sublevel, supply_raw)
+		return bom_wise_item_details
+
+						
 
 	def make_items_dict(self, item_list):
 		for i in item_list:
@@ -451,8 +465,10 @@ class ProductionPlanningTool(Document):
 					"company": self.company,
 					"requested_by": frappe.session.user
 				})
-				material_request.update({"material_request_type": item_wrapper.default_material_request_type})
-
+				if item_wrapper.default_bom:
+					material_request.update({"material_request_type": "Manufacture"}) 
+				else:
+					material_request.update({"material_request_type": "Purchase"})
 				for sales_order, requested_qty in items_to_be_requested[item].items():
 					material_request.append("items", {
 						"doctype": "Material Request Item",
